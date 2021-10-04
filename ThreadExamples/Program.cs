9413701eba863c1a2cca21ddf49bc9d0aae47917
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-10-04 11:19:36 +0800

    More Thread Examples

--- ThreadExamples/Program.cs
+++ ThreadExamples/Program.cs
@@ -15,19 +15,54 @@ namespace ThreadExamples
     {
         static void Main(string[] args)
         {
+            #region EX 1
             MyClass myClass = new MyClass();
-            myClass.InitialiseThread();
+            //myClass.InitialiseThread();
 
-            //another process to do
-            for(int i = 0; i<1000; i++)
-            {
-                Console.WriteLine("Thread in main");
-                //Thread.Sleep(5000);
+            ////another process to do
+            //for(int i = 0; i<1000; i++)
+            //{
+            //    Console.WriteLine("Thread in main");
+            //    //Thread.Sleep(5000);
 
 
-            }
+            //}
+            #endregion
+
+
+            ThreadExample t = new ThreadExample();
+
+            Console.WriteLine("Nomal display");
+            t.Display(); //syncronous call => no thread
+
+            Console.WriteLine("Async display");
+            t.DisplayAsync(); //=> running async with thread
 
             Console.ReadLine();
+        }
+    }
+    class ThreadExample
+    {
+        public void Display()
+        {
+            Console.WriteLine("In Display method");
+        }
+        public void DisplayAsync()
+        {
+            Console.WriteLine("In Display Async");
+
+            //method 1
+            Thread t = new Thread(() => { Display(); });
+
+            //method 2
+            ThreadStart start = new ThreadStart(Display);
+            Thread t1 = new Thread(start);
+
+            Console.WriteLine("Before start thread");
+            t.Start();
+            t1.Start();
+
+            Console.WriteLine("end of thread");
         }
     }
 
